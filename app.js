2f3d36bda24ef33220270f1a47faea43cb9c7586
andrsand <angorko@list.ru>
2023-04-11 08:02:56 +0300

    Uploading to express and multer project

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const multer = require("multer");
 const app = express();
 
 app.use(express.static(__dirname));
-app.use(multer({ dest: "uploads" }).single("filedata"));
+app.use(multer({ dest: "uploads" }).single("filedata")); //  multer добавляется в виде компонента middleware.  Передается объект с параметром dest указывающим путь для загрузки файлов. В данном случае это папка uploads:
+//функция single() указывает, что загружаться будет один файл. На форме в index.html есть поле для загрузки одного файла с именем filedata. В single передается название поля, которое используется на форме для загрузки файла.
 app.post("/upload", function (req, res, next) {
 
     let filedata = req.file;
