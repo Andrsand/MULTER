5d4e5493733223bbc4f4d0265d01858f0e10d4c7
andrsand <angorko@list.ru>
2023-04-11 22:16:57 +0300

    Setting File Save Options

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const multer = require("multer");
 
 const app = express();
 
+const storageConfig = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "uploads");
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
 app.use(express.static(__dirname));
-app.use(multer({ dest: "uploads" }).single("filedata")); //  multer добавляется в виде компонента middleware.  Передается объект с параметром dest указывающим путь для загрузки файлов. В данном случае это папка uploads:
-//функция single() указывает, что загружаться будет один файл. На форме в index.html есть поле для загрузки одного файла с именем filedata. В single передается название поля, которое используется на форме для загрузки файла.
+
+app.use(multer({ storage: storageConfig }).single("filedata"));
 app.post("/upload", function (req, res, next) {
 
     let filedata = req.file;
-    console.log(filedata);
     if (!filedata)
         res.send("Ошибка при загрузке файла");
     else
         res.send("Файл загружен");
 });
-app.listen(3000);
+app.listen(3000, () => { console.log("Server started"); });
